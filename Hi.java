4d3f8d325d45ae2f1ab6a9b42e7cccdebfe5b507
Zakhar-S1 <43477527+Zakhar-S1@users.noreply.github.com>
2020-02-28 10:26:50 +0300

    Hi #2

--- Hi.java
+++ Hi.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
+
 class HelloWorld {
     public static void main(String[] args) {
-        int x = 5;
+        
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input a number: ");
+        
         int y = 6;
+        int x = in.nextInt();
+          
         System.out.println("Hello World!");
         Sysyem.out.println("Goodbye World!");
         System.out.println("x=" + x + "; y=" + y);
+        
+        in.close();
     }
 }
