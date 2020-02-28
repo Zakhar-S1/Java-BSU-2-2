bda46283ae0751749b3821405e1c1c46f43b70b4
Zakhar-S1 <43477527+Zakhar-S1@users.noreply.github.com>
2020-02-28 10:23:04 +0300

    Hi #1

--- Hi.java
+++ Hi.java
@@ -1,5 +1,9 @@
 class HelloWorld {
     public static void main(String[] args) {
+        int x = 5;
+        int y = 6;
         System.out.println("Hello World!");
+        Sysyem.out.println("Goodbye World!");
+        System.out.println("x=" + x + "; y=" + y);
     }
 }
